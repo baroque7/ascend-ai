00abb5972501add5e81f3bce799a192d3217bed9
baroque7 <methe596@gmail.com>
2026-05-14 12:33:36 +0100

    fix: remove unused currentPage prop from BottomNav

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
 import BottomNav from '@/components/dashboard/BottomNav';
 
 export default function DashboardLayout({
@@ -8,17 +7,13 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-
   return (
     <div className="min-h-screen bg-[#000000] text-[#f8f8ff]">
-      {/* Main content */}
       <main className="relative z-10 pb-20">
         {children}
       </main>
 
-      {/* Bottom Navigation */}
-      <BottomNav currentPage={pathname.split('/').pop()} />
+      <BottomNav />
     </div>
   );
 }
